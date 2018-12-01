71877e3cc19ddd4c136008fbb9f35bc1a3e5509e
Walerian <wsobczak@zendesk.com>
2018-12-01 23:36:51 +0100

    Day 1. Part 2.

--- 2018/day_1/ChronalCalibration.hs
+++ 2018/day_1/ChronalCalibration.hs
@@ -1,3 +1,4 @@
+-- https://adventofcode.com/2018/day/1
 import System.Environment
 
 trimPlus :: String -> String
@@ -6,7 +7,22 @@ trimPlus = filter (/= '+')
 parseArgs :: [String] -> [Integer]
 parseArgs = map (read . trimPlus)
 
+getIntArgs :: IO [Integer]
+getIntArgs = fmap parseArgs $ getArgs
+
+reachedTwice :: [Integer] -> Integer
+reachedTwice xs = reachedTwice' [0] xs xs
+
+reachedTwice' :: [Integer] -> [Integer] -> [Integer] -> Integer
+reachedTwice' sums [] list = reachedTwice' sums list list
+reachedTwice' (x:xs) (y:ys) list =
+  if elem sum (x:xs)
+    then sum
+    else reachedTwice' (sum:x:xs) ys list
+  where sum = x + y
+
 main :: IO ()
 main = do
-  frequencies <- getArgs
-  print $ sum (parseArgs frequencies)
+  args <- getIntArgs
+  print $ sum args
+  print $ reachedTwice args
