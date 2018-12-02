5354a20944fd8068c74cc62c7c3026893ff33e8f
Walerian <wsobczak@zendesk.com>
2018-12-02 11:51:26 +0100

    Day 2. Part 2.

--- 2018/day-2/InventoryManagementSystem.hs
+++ 2018/day-2/InventoryManagementSystem.hs
@@ -2,16 +2,16 @@
 
 module InventoryManagementSystem where
 
-import Data.List (group, sort)
+import Data.List (group, sort, intersect, (\\))
 
 readLines :: String -> IO [String]
 readLines = fmap lines . readFile
 
 calculateChecksum :: [String] -> Int
-calculateChecksum input = countTwos * countThrees
+calculateChecksum list = countTwos * countThrees
   where
-  countTwos = countOccurences 2 input
-  countThrees = countOccurences 3 input
+  countTwos = countOccurences 2 list
+  countThrees = countOccurences 3 list
 
 countOccurences :: Int -> [String] -> Int
 countOccurences value list = length (filter (any (== value)) (map toOccurences list))
@@ -19,7 +19,14 @@ countOccurences value list = length (filter (any (== value)) (map toOccurences l
 toOccurences :: String -> [Int]
 toOccurences = map length . group . sort
 
+commonLetters :: [String] -> String
+commonLetters list = head [intersect xs ys | xs <- list, ys <- list, differByOneCharacter xs ys]
+
+differByOneCharacter :: String -> String -> Bool
+differByOneCharacter xs ys = length (xs \\ ys) == 1
+
 main :: IO ()
 main = do
   input <- readLines "input"
   print $ calculateChecksum input
+  print $ commonLetters input
