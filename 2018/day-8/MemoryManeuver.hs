c328aa6c199fa5fdecc8fed4d7d1771dfa254d93
Walerian <wsobczak@zendesk.com>
2018-12-09 14:34:44 +0100

    Day 8. Part 2.

--- 2018/day-8/MemoryManeuver.hs
+++ 2018/day-8/MemoryManeuver.hs
@@ -33,7 +33,17 @@ sumMetadata :: Tree -> Metadata
 sumMetadata (Tree [] metadata) = sum metadata
 sumMetadata (Tree subtrees metadata) = sum metadata + sum (map sumMetadata subtrees)
 
+treeValue :: Tree -> Metadata
+treeValue (Tree [] metadata) = sum metadata
+treeValue (Tree subtrees metadata) = sum (map metadataValue metadata)
+  where
+  metadataValue :: Metadata -> Metadata
+  metadataValue x
+    | x <= length subtrees = (map treeValue subtrees) !! (x - 1)
+    | otherwise = 0
+
 main :: IO ()
 main = do
   tree <- fmap toTree (readInts "input")
   print $ sumMetadata tree
+  print $ treeValue tree
