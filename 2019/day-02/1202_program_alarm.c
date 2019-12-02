b5913e83cf01905a896fe3efc0debc073f88c677
Walerian <wsobczak@zendesk.com>
2019-12-02 20:37:07 +0100

    Day 2. Part 2.

--- 2019/day-02/1202_program_alarm.c
+++ 2019/day-02/1202_program_alarm.c
@@ -1,40 +1,65 @@
-// https://adventofcode.com/2019/day/1
+// https://adventofcode.com/2019/day/2
 
 #include <stdio.h>
 
 #define INPUT_SIZE 137
-#define ADD_CODE 1
-#define MULTIPLY_CODE 2
-#define HALT_CODE 99
+#define ADD_INSTRUCTION 1
+#define MULTIPLY_INSTRUCTION 2
+#define HALT_INSTRUCTION 99
+
+#define NOUN_INDEX 1
+#define VERB_INDEX 2
+#define EXPECTED_RESULT 19690720
 
 int main() {
   FILE *inputFile;
   inputFile = fopen("input", "r");
 
-  int codes[INPUT_SIZE];
+  int memory[INPUT_SIZE];
+  int workingMemory[INPUT_SIZE];
 
   for (int i = 0; i < INPUT_SIZE; i++) {
-    fscanf(inputFile, "%d,", &codes[i]);
+    fscanf(inputFile, "%d,", &memory[i]);
   }
 
-  int firstInputPosition, secondInputPosition, resultPosition, currentPosition = 0;
-  int currentOptcode = currentOptcode = codes[currentPosition];
+  int opcode, instructionPointer;
+  int firstParameterAddress, secondParameterAddress, resultAddress;
+  int noun, verb;
 
-  while (currentOptcode != HALT_CODE) {
-    currentOptcode = codes[currentPosition];
-    firstInputPosition = codes[currentPosition + 1];
-    secondInputPosition = codes[currentPosition + 2];
-    resultPosition = codes[currentPosition + 3];
+  for (noun = 0; noun < 100; noun++) {
+    for (verb = 0; verb < 100; verb++) {
+      for(int i = 0; i < INPUT_SIZE; i++) {
+        workingMemory[i] = memory[i];
+      }
 
-    if (currentOptcode == ADD_CODE) {
-      codes[resultPosition] = codes[firstInputPosition] + codes[secondInputPosition];
-    } else if (currentOptcode == MULTIPLY_CODE) {
-      codes[resultPosition] = codes[firstInputPosition] * codes[secondInputPosition];
-    };
+      workingMemory[NOUN_INDEX] = noun;
+      workingMemory[VERB_INDEX] = verb;
 
-    currentPosition += 4;
+      instructionPointer = 0;
+      opcode = workingMemory[instructionPointer];
+
+      while (opcode != HALT_INSTRUCTION) {
+        opcode = workingMemory[instructionPointer];
+        firstParameterAddress = workingMemory[instructionPointer + 1];
+        secondParameterAddress = workingMemory[instructionPointer + 2];
+        resultAddress = workingMemory[instructionPointer + 3];
+
+        if (opcode == ADD_INSTRUCTION) {
+          workingMemory[resultAddress] = workingMemory[firstParameterAddress] + workingMemory[secondParameterAddress];
+        } else if (opcode == MULTIPLY_INSTRUCTION) {
+          workingMemory[resultAddress] = workingMemory[firstParameterAddress] * workingMemory[secondParameterAddress];
+        };
+
+        instructionPointer += 4;
+      }
+
+      if (workingMemory[0] == EXPECTED_RESULT) {
+        printf("%d\n%d\n", noun, verb);
+        return 0;
+      }
+    }
   }
 
-  printf("%d\n", codes[0]);
+  printf("Not found!\n");
   return 0;
 }
