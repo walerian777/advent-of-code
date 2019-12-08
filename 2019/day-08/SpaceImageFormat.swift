901997f7f4022a69f45224b613afde8b577c52a2
Walerian <wsobczak@zendesk.com>
2019-12-08 23:06:33 +0100

    Day 8. Part 2.

--- 2019/day-08/SpaceImageFormat.swift
+++ 2019/day-08/SpaceImageFormat.swift
@@ -16,6 +16,7 @@ func loadImage() -> [[Int]] {
   return chunks
 }
 
+// Part 1
 func occurences(of digit: Int, in layer: [Int]) -> Int {
   return layer.filter { $0 == digit }.count
 }
@@ -26,7 +27,60 @@ func imageChecksum(image: [[Int]]) -> Int {
     .last!
   let numberOfOneDigits = occurences(of: 1, in: layerWithFewestZeros)
   let numberOfOwoDigits = occurences(of: 2, in: layerWithFewestZeros)
+
   return numberOfOneDigits * numberOfOwoDigits
 }
 
 print(imageChecksum(image: loadImage()))
+
+// Part 2
+enum Pixel: Int, CustomStringConvertible {
+  case black, white, transparent
+
+  func stack(with pixel: Pixel) -> Pixel {
+    switch self {
+    case .black:
+      return self
+    case .white:
+      return self
+    case .transparent:
+      return pixel
+    }
+  }
+
+  func render() -> String {
+    return description
+  }
+
+  var description: String {
+    get {
+      switch self {
+      case .black:
+        return "⬛️"
+      case .white:
+        return "⬜️"
+      case .transparent:
+        return "🀆"
+      }
+    }
+  }
+}
+
+
+func pixelsTransposition(of image: [[Int]]) -> [[Pixel]] {
+  return image.first!.indices
+    .map { layer in image.map { $0[layer] }.map { Pixel(rawValue: $0)! } }
+}
+
+func renderImage(of pixels: [[Pixel]]) -> [[String]] {
+  let image = pixels.map { $0.reduce(Pixel.transparent, { $0.stack(with: $1) }) }
+  let rows: [[String]] = stride(from: 0, to: image.count, by: width).map {
+    Array(image[$0 ..< Swift.min($0 + width, image.count)]).map { $0.render() }
+  }
+
+  return rows
+}
+
+for row in renderImage(of: pixelsTransposition(of: loadImage())) {
+  print(row.joined())
+}
