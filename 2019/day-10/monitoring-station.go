bf77b19eb78f86d72d364439493c4a468c7e0a02
Walerian <wsobczak@zendesk.com>
2019-12-11 12:10:32 +0100

    Day 10. Part 2.

--- 2019/day-10/monitoring-station.go
+++ 2019/day-10/monitoring-station.go
@@ -6,6 +6,8 @@ import (
   "fmt"
   "io/ioutil"
   "strings"
+  "math"
+  "sort"
 )
 
 func main() {
@@ -19,6 +21,9 @@ func main() {
   columns_count := len(asteroids[0])
 
   max_detected_asteroids := 0
+  detected_asteroids := map[Coordinate]bool{}
+  station_row := 0
+  station_column := 0
 
   for i := 0; i < rows_count; i++ {
     for j := 0; j < columns_count; j++ {
@@ -26,7 +31,7 @@ func main() {
         break
       }
 
-      visited_asteroids := map[Coordinates]bool{}
+      visited_asteroids := map[Coordinate]bool{}
       for row := 0; row < rows_count; row++ {
         for column := 0; column < columns_count; column++ {
           if asteroids[row][column] == '#' && (row != i || column != j) {
@@ -34,7 +39,7 @@ func main() {
             column_diff := column - j
             divisor := gcd(row_diff, column_diff)
 
-            coordinates := Coordinates{row_diff / divisor, column_diff / divisor}
+            coordinates := Coordinate{row_diff / divisor, column_diff / divisor}
 
             visited_asteroids[coordinates] = true
           }
@@ -43,15 +48,45 @@ func main() {
 
       if len(visited_asteroids) > max_detected_asteroids {
         max_detected_asteroids = len(visited_asteroids)
+        detected_asteroids = visited_asteroids
+        station_row = i
+        station_column = j
       }
     }
   }
 
   fmt.Println(max_detected_asteroids)
+
+  var candidates []Candidate
+  for coordinate := range detected_asteroids {
+    key := atan(coordinate.a, coordinate.b)
+    candidate := Candidate{key, coordinate}
+
+    candidates = append(candidates, candidate)
+  }
+
+  sort.Slice(candidates, func(i, j int) bool {
+    return candidates[i].key < candidates[j].key
+  })
+
+
+  winner_row := candidates[199].coordinate.a
+  winner_column := candidates[199].coordinate.b
+
+  row := station_row - winner_row
+  column := station_column + winner_column
+
+  fmt.Println(column * 100 + row)
 }
 
-type Coordinates struct {
-  a, b interface{}
+type Coordinate struct {
+  a int
+  b int
+}
+
+type Candidate struct {
+  key float64
+  coordinate Coordinate
 }
 
 func abs(a int) int {
@@ -67,6 +102,16 @@ func gcd(a, b int) int {
     return abs(b)
   } else {
     return gcd(b % a, a)
+  }
+}
+
+func atan(a, b int) float64 {
+  value := math.Atan2(float64(a), float64(b))
+
+  if value > math.Pi / 2.0 {
+    return value - math.Pi / 2.0
+  } else {
+    return value
   }
 }
 
