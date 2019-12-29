ba98678ece7991d9fe27e41318f13f4049400b44
Walerian <wsobczak@zendesk.com>
2019-12-29 01:47:52 +0100

    Day 1. Part 2.

--- 2015/README.md
+++ 2015/README.md
@@ -1,3 +1,3 @@
 ### Day 1
 * Puzzle: [Not Quite Lisp](https://adventofcode.com/2015/day/1)
-* Solution: [`not_quite_list.rs`](day-01/not_quite_list.rs)
+* Solution: [`not_quite_lisp.rs`](day-01/not_quite_lisp.rs)
--- 2015/day-01/not_quite_lisp.rs
+++ 2015/day-01/not_quite_lisp.rs
@@ -5,13 +5,18 @@ fn main() {
         .expect("Oops");
     let instructions = input.trim_end();
     let mut floor = 0;
+    let mut basement_position = 0;
 
-    for instruction in instructions.chars() {
+    for (index, instruction) in instructions.chars().enumerate() {
         match instruction {
             '(' => { floor += 1 },
             ')' => { floor -= 1 },
             _ => println!("Ooops")
         }
+        if basement_position == 0 && floor == -1 {
+            basement_position = index + 1
+        }
     }
     println!("{:?}", floor);
+    println!("{:?}", basement_position);
 }
