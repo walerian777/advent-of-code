1505c957351c86f9748d3ede5422a38fb3b1e6d5
Walerian <wsobczak@zendesk.com>
2019-12-30 21:34:33 +0100

    Day 4. Part 1.

---
+++ 2015/day-04/ideal_stocking_stuffer.rs
@@ -0,0 +1,24 @@
+// https://adventofcode.com/2015/day/4
+
+use crypto::digest::Digest;
+use crypto::md5::Md5;
+
+fn main() {
+    let secret_key = "ckczppom";
+    let mut number = 1;
+    let mut hasher = Md5::new();
+
+    loop {
+        let hash = format!("{}{}", secret_key, number.to_string());
+        hasher.input_str(&hash);
+        let digest = hasher.result_str();
+
+        if digest.starts_with("00000") {
+            break;
+        }
+        number += 1;
+        hasher.reset();
+    }
+
+    println!("{:?}", number);
+}
