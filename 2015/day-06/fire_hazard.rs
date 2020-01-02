bb9a820bde46742794eb7ff2155470e576076a11
Walerian <wsobczak@zendesk.com>
2020-01-02 11:52:34 +0100

    Day 6. Part 2.

--- 2015/day-06/fire_hazard.rs
+++ 2015/day-06/fire_hazard.rs
@@ -8,27 +8,33 @@ fn main() {
     let instructions: Vec<&str> = input.trim_end().split("\n").collect();
 
     let mut lights = HashMap::new();
+    let mut elvish_lights = HashMap::new();
 
     for x in 0..=999 {
         for y in 0..=999 {
             lights.insert((x, y), false);
+            elvish_lights.insert((x, y), 0);
         }
     }
 
     for instruction in instructions {
         if instruction.starts_with("turn on") {
             let coordinates = parse_instruction(instruction, "turn on");
-            execute_command(&mut lights, turn_on, &coordinates[0], &coordinates[1])
+            execute_command(&mut lights, turn_on, &coordinates[0], &coordinates[1]);
+            execute_elvish_command(&mut elvish_lights, elvish_turn_on, &coordinates[0], &coordinates[1]);
         } else if instruction.starts_with("turn off") {
             let coordinates = parse_instruction(instruction, "turn off");
-            execute_command(&mut lights, turn_off, &coordinates[0], &coordinates[1])
+            execute_command(&mut lights, turn_off, &coordinates[0], &coordinates[1]);
+            execute_elvish_command(&mut elvish_lights, elvish_turn_off, &coordinates[0], &coordinates[1]);
         } else if instruction.starts_with("toggle") {
             let coordinates = parse_instruction(instruction, "toggle");
-            execute_command(&mut lights, toggle, &coordinates[0], &coordinates[1])
+            execute_command(&mut lights, toggle, &coordinates[0], &coordinates[1]);
+            execute_elvish_command(&mut elvish_lights, elvish_toggle, &coordinates[0], &coordinates[1]);
         }
     }
 
     println!("{:?}", lights.values().filter(|&state| *state).count());
+    println!("{:?}", elvish_lights.values().fold(0, |acc, state| acc + state));
 }
 
 fn parse_instruction(instruction: &str, command: &str) -> Vec<Vec<u32>> {
@@ -54,6 +60,20 @@ fn execute_command(
     }
 }
 
+
+fn execute_elvish_command(
+    elvish_lights: &mut HashMap<(u32, u32), u32>,
+    elvish_command: fn(&mut u32),
+    start_range: &Vec<u32>,
+    end_range: &Vec<u32>
+    ) {
+    for x in start_range[0]..=end_range[0] {
+        for y in start_range[1]..=end_range[1] {
+            elvish_lights.entry((x, y)).and_modify(elvish_command);
+        }
+    }
+}
+
 fn turn_on(state: &mut bool) {
     *state = true
 }
@@ -64,4 +84,16 @@ fn turn_off(state: &mut bool) {
 
 fn toggle(state: &mut bool) {
     *state = !*state
+}
+
+fn elvish_turn_on(state: &mut u32) {
+    *state += 1
+}
+
+fn elvish_turn_off(state: &mut u32) {
+    *state = state.saturating_sub(1)
+}
+
+fn elvish_toggle(state: &mut u32) {
+    *state += 2
 }
