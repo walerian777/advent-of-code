439cac0174e3967008e33213fac325441efaddea
Walerian <wsobczak@zendesk.com>
2020-09-13 11:02:50 +0200

    2016: Day 2. Part 2.

--- 2016/README.md
+++ 2016/README.md
@@ -4,3 +4,7 @@ The year of Clojure
 ### Day 1
 * Puzzle: [No Time for a Taxicab](https://adventofcode.com/2016/day/1)
 * Solution: [`no_time_for_a_taxicab.clj`](day-01/no_time_for_a_taxicab.clj)
+
+### Day 2
+* Puzzle: [Bathroom Security](https://adventofcode.com/2016/day/2)
+* Solution: [`bathroom_security.clj`](day-02/bathroom_security.clj)
--- 2016/day-02/bathroom_security.clj
+++ 2016/day-02/bathroom_security.clj
@@ -8,7 +8,7 @@
 (def instructions
   (map #(split % #"") input))
 
-(def keypad
+(def regular-keypad
   {1 {"U" 1 "L" 1 "R" 2 "D" 4}
    2 {"U" 2 "L" 1 "R" 3 "D" 5}
    3 {"U" 3 "L" 2 "R" 3 "D" 6}
@@ -21,7 +21,7 @@
 
 (def starting-key 5)
 
-(defn find-code []
+(defn find-code [keypad]
   (map
    (fn [instructions-row]
      (reduce
@@ -31,4 +31,21 @@
       instructions-row))
    instructions))
 
-(println (find-code))
+(println (find-code regular-keypad))
+
+(def weird-keypad
+  {1 {"U" 1 "L" 1 "R" 1 "D" 3}
+   2 {"U" 2 "L" 2 "R" 3 "D" 6}
+   3 {"U" 1 "L" 2 "R" 4 "D" 7}
+   4 {"U" 4 "L" 3 "R" 4 "D" 8}
+   5 {"U" 5 "L" 5 "R" 6 "D" 5}
+   6 {"U" 2 "L" 5 "R" 7 "D" "A"}
+   7 {"U" 3 "L" 6 "R" 8 "D" "B"}
+   8 {"U" 4 "L" 7 "R" 9 "D" "C"}
+   9 {"U" 9 "L" 8 "R" 9 "D" 9}
+   "A" {"U" 6 "L" "A" "R" "B" "D" "A"}
+   "B" {"U" 7 "L" "A" "R" "C" "D" "D"}
+   "C" {"U" 8 "L" "B" "R" "C" "D" "C"}
+   "D" {"U" "B" "L" "D" "R" "D" "D" "D"}})
+
+(println (find-code weird-keypad))
