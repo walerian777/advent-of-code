cc8ef869fbcbee969ce80cb0b5685c3379e0767a
Walerian <wsobczak@indeed.com>
2024-11-04 18:40:02 +0900

    2023: Day 1. Part 2.

--- 2023/day-01/trebuchet.go
+++ 2023/day-01/trebuchet.go
@@ -7,7 +7,36 @@ import (
 	"strconv"
 )
 
+var alphaDigits = [...]string{
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+}
+
+var alphaDigitsReversed = [...]string{
+	"eno",
+	"owt",
+	"eerht",
+	"ruof",
+	"evif",
+	"xis",
+	"neves",
+	"thgie",
+	"enin",
+}
+
 func main() {
+	fmt.Println(trebuchet())
+
+}
+
+func trebuchet() int {
 	file, err := os.Open("input")
 	if err != nil {
 		fmt.Println(err)
@@ -20,28 +49,56 @@ func main() {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		calibration := 0
 
+	forward:
 		for i := 0; i < len(line); i++ {
 			val, err := strconv.Atoi(string(line[i]))
 			if err == nil {
-				calibration += val * 10
+				sum += val * 10
 				break
 			}
+			for j, d := range alphaDigits {
+				if d[0] != line[i] {
+					continue
+				}
+				for k := 0; k < len(d); k++ {
+					if i+k >= len(line) || d[k] != line[i+k] {
+						break
+					}
+					if k+1 == len(d) {
+						sum += (j + 1) * 10
+						break forward
+					}
+				}
+			}
 		}
+	backward:
 		for i := len(line) - 1; i >= 0; i-- {
 			val, err := strconv.Atoi(string(line[i]))
 			if err == nil {
-				calibration += val
+				sum += val
 				break
 			}
+			for j, d := range alphaDigitsReversed {
+				if d[0] != line[i] {
+					continue
+				}
+				for k := 0; k < len(d); k++ {
+					if i-k <= 0 || d[k] != line[i-k] {
+						break
+					}
+					if k+1 == len(d) {
+						sum += (j + 1)
+						break backward
+					}
+				}
+			}
 		}
-		sum += calibration
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 		panic("scanner error")
 	}
 
-	fmt.Println(sum)
+	return sum
 }
