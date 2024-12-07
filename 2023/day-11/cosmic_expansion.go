d2e7930dd6b177ccdd133e2672d1fe064a2e5d7e
Walerian <waleria.sobczak@gmail.com>
2024-12-08 07:56:55 +0900

    2023: Day 11. Part 2.

--- 2023/day-11/cosmic_expansion.go
+++ 2023/day-11/cosmic_expansion.go
@@ -71,7 +71,58 @@ func part1() int {
 }
 
 func part2() int {
-	return 0
+	file, err := os.Open("input")
+	if err != nil {
+		panic("cannot open input")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var galaxies []Coord
+	regularXs := make(map[int]bool)
+	regularYs := make(map[int]bool)
+	var x, y int
+	for scanner.Scan() {
+		line := []byte(scanner.Text())
+		y = 0
+		for _, c := range line {
+			if c == '#' {
+				galaxies = append(galaxies, Coord{x, y})
+				regularYs[y] = true
+				regularXs[x] = true
+			}
+			y++
+		}
+		x++
+	}
+
+	var adjustedGalaxies []Coord
+	for _, g := range galaxies {
+		newX := g.x
+		newY := g.y
+		for i := 0; i < g.x; i++ {
+			if _, ok := regularXs[i]; !ok {
+				newX += 1000000 - 1
+			}
+		}
+		for j := 0; j < g.y; j++ {
+			if _, ok := regularYs[j]; !ok {
+				newY += 1000000 - 1
+			}
+		}
+		adjustedGalaxies = append(adjustedGalaxies, Coord{newX, newY})
+	}
+
+	var sum int
+	for i := 0; i < len(adjustedGalaxies); i++ {
+		for j := i + 1; j < len(adjustedGalaxies); j++ {
+			sc := manhattanDistance(adjustedGalaxies[i], adjustedGalaxies[j])
+			sum += sc
+		}
+	}
+
+	return sum
 }
 
 func manhattanDistance(p, q Coord) int {
