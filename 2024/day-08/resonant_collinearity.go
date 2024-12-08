9ca7c7a811b4860ebbcef83e2f32695c974dd4a3
Walerian <waleria.sobczak@gmail.com>
2024-12-08 15:18:42 +0900

    2024: Day 8. Part 2.

--- 2024/day-08/resonant_collinearity.go
+++ 2024/day-08/resonant_collinearity.go
@@ -74,5 +74,56 @@ func part1() int {
 }
 
 func part2() int {
-	return 0
+	file, err := os.Open("input")
+	if err != nil {
+		panic("cannot open input")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var x int
+	var grid [][]byte
+	antennas := make(map[byte][]Coord)
+	for scanner.Scan() {
+		line := []byte(scanner.Text())
+		grid = append(grid, line)
+		for y, c := range line {
+			if c == '.' {
+				continue
+			}
+			antennas[c] = append(antennas[c], Coord{x, y})
+		}
+		x++
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic("scanner error")
+	}
+
+	maxX := len(grid) - 1
+	maxY := len(grid[0]) - 1
+
+	antinodes := make(map[Coord]bool)
+	for _, as := range antennas {
+		for i, a := range as {
+			antinodes[a] = true
+			for _, b := range as[i+1:] {
+				diff1 := a.sub(b)
+				an1 := a.add(diff1)
+				for an1.x >= 0 && an1.x <= maxX && an1.y >= 0 && an1.y <= maxY {
+					antinodes[an1] = true
+					an1 = an1.add(diff1)
+				}
+				diff2 := b.sub(a)
+				an2 := b.add(diff2)
+				for an2.x >= 0 && an2.x <= maxX && an2.y >= 0 && an2.y <= maxY {
+					antinodes[an2] = true
+					an2 = an2.add(diff2)
+				}
+			}
+		}
+	}
+
+	return len(antinodes)
 }
