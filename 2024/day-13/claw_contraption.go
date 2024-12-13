24da5eee1fd17b7d801cd8615dd1c1c3ca0d5bf1
Walerian <waleria.sobczak@gmail.com>
2024-12-14 07:49:12 +0900

    2024: Day 13. Part 2.

--- 2024/day-13/claw_contraption.go
+++ 2024/day-13/claw_contraption.go
@@ -14,14 +14,14 @@ func main() {
 }
 
 type Config struct {
-	a, b, p [2]int
+	a, b, p [2]int64
 }
 
 func (c Config) String() string {
 	return fmt.Sprintf("[A]: X+%d, Y+%d | [B]: X+%d, Y+%d | X=%d, Y=%d", c.a[0], c.a[1], c.b[0], c.b[1], c.p[0], c.p[1])
 }
 
-func (c *Config) Tokens() int {
+func (c *Config) Tokens() int64 {
 	det := c.a[0]*c.b[1] - c.a[1]*c.b[0]
 	if det == 0 {
 		return 0
@@ -35,7 +35,7 @@ func (c *Config) Tokens() int {
 	return 0
 }
 
-func part1() int {
+func part1() int64 {
 	file, err := os.Open("input")
 	if err != nil {
 		panic("cannot open file")
@@ -67,7 +67,7 @@ func part1() int {
 			if err != nil {
 				panic("atoi")
 			}
-			currentCfg.a = [2]int{x, y}
+			currentCfg.a = [2]int64{int64(x), int64(y)}
 		case 'B':
 			b := reButton.FindAllStringSubmatch(line, 1)[0]
 			x, err := strconv.Atoi(b[1])
@@ -78,7 +78,7 @@ func part1() int {
 			if err != nil {
 				panic("atoi")
 			}
-			currentCfg.b = [2]int{x, y}
+			currentCfg.b = [2]int64{int64(x), int64(y)}
 		case 'X':
 			b := rePrize.FindAllStringSubmatch(line, 1)[0]
 			x, err := strconv.Atoi(b[1])
@@ -89,7 +89,7 @@ func part1() int {
 			if err != nil {
 				panic("atoi")
 			}
-			currentCfg.p = [2]int{x, y}
+			currentCfg.p = [2]int64{int64(x), int64(y)}
 		}
 	}
 
@@ -97,13 +97,78 @@ func part1() int {
 		panic("scanner error")
 	}
 
-	var sum int
+	var sum int64
 	for _, cfg := range configs {
 		sum += cfg.Tokens()
 	}
 	return sum
 }
 
-func part2() int {
-	return 0
+func part2() int64 {
+	file, err := os.Open("input")
+	if err != nil {
+		panic("cannot open file")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	reButton := regexp.MustCompile(`Button.+X\+(\d+), Y\+(\d+)`)
+	rePrize := regexp.MustCompile(`Prize.+X=(\d+), Y=(\d+)`)
+
+	var configs []Config
+	currentCfg := Config{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			configs = append(configs, currentCfg)
+			currentCfg = Config{}
+			continue
+		}
+		switch line[7] {
+		case 'A':
+			a := reButton.FindAllStringSubmatch(line, 1)[0]
+			x, err := strconv.Atoi(a[1])
+			if err != nil {
+				panic("atoi")
+			}
+			y, err := strconv.Atoi(a[2])
+			if err != nil {
+				panic("atoi")
+			}
+			currentCfg.a = [2]int64{int64(x), int64(y)}
+		case 'B':
+			b := reButton.FindAllStringSubmatch(line, 1)[0]
+			x, err := strconv.Atoi(b[1])
+			if err != nil {
+				panic("atoi")
+			}
+			y, err := strconv.Atoi(b[2])
+			if err != nil {
+				panic("atoi")
+			}
+			currentCfg.b = [2]int64{int64(x), int64(y)}
+		case 'X':
+			b := rePrize.FindAllStringSubmatch(line, 1)[0]
+			x, err := strconv.Atoi(b[1])
+			if err != nil {
+				panic("atoi")
+			}
+			y, err := strconv.Atoi(b[2])
+			if err != nil {
+				panic("atoi")
+			}
+			currentCfg.p = [2]int64{int64(x) + 10000000000000, int64(y) + 10000000000000}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic("scanner error")
+	}
+
+	var sum int64
+	for _, cfg := range configs {
+		sum += cfg.Tokens()
+	}
+	return sum
 }
