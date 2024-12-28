e9b96347275c5e57ea3cb4251819d39fd4779ae4
Walerian <waleria.sobczak@gmail.com>
2024-12-28 15:10:55 +0900

    2023: Day 12. Part 2.

--- 2023/day-12/hot_springs.go
+++ 2023/day-12/hot_springs.go
@@ -10,12 +10,13 @@ import (
 
 func main() {
 	fmt.Println(part1())
+	fmt.Println(part2())
 }
 
 const (
-	DAMAGED     string = "#"
-	OPERATIONAL string = "."
-	UNKNOWN     string = "?"
+	DAMAGED     byte = '#'
+	OPERATIONAL byte = '.'
+	UNKNOWN     byte = '?'
 )
 
 type ConditionRecord struct {
@@ -23,40 +24,62 @@ type ConditionRecord struct {
 	Groups  []int
 }
 
+type State struct {
+	CurrentGroup int
+	DamagedCount int
+}
+
+func (s State) NextGroup() State {
+	return State{s.CurrentGroup + 1, 0}
+}
+
+func (s State) IncrementDamage() State {
+	return State{s.CurrentGroup, s.DamagedCount + 1}
+}
+
 func (r *ConditionRecord) String() string {
 	return fmt.Sprintf("%v %v", r.Springs, r.Groups)
 }
 
 func (r *ConditionRecord) Arrangements() int {
-	return r.FindArrangements(r.Springs)
+	arrangements := make(map[State]int)
+	arrangements[State{}] = 1
+	for i := range r.Springs {
+		newArrangements := make(map[State]int)
+		for s, v := range arrangements {
+			if r.Springs[i] != DAMAGED {
+				if s.DamagedCount == 0 {
+					newArrangements[s] += v
+
+				} else if s.DamagedCount == r.Groups[s.CurrentGroup] {
+					newArrangements[s.NextGroup()] += v
+				}
+			}
+			if r.Springs[i] != OPERATIONAL {
+				if s.CurrentGroup < len(r.Groups) && s.DamagedCount < r.Groups[s.CurrentGroup] {
+					newArrangements[s.IncrementDamage()] += v
+				}
+			}
+		}
+		arrangements = newArrangements
+	}
+	var sum int
+	for s, v := range arrangements {
+		if r.IsValid(s) {
+			sum += v
+		}
+	}
+	return sum
 }
 
-func (r *ConditionRecord) FindArrangements(curr string) int {
-	if strings.Index(curr, UNKNOWN) < 0 {
-		if r.IsValid(curr) {
-			return 1
-		}
-		return 0
+func (r *ConditionRecord) IsValid(s State) bool {
+	if s.CurrentGroup == len(r.Groups) && s.DamagedCount == 0 {
+		return true
 	}
-	dmgd := strings.Replace(curr, UNKNOWN, DAMAGED, 1)
-	optl := strings.Replace(curr, UNKNOWN, OPERATIONAL, 1)
-	return r.FindArrangements(dmgd) + r.FindArrangements(optl)
-}
-
-func (r *ConditionRecord) IsValid(arrangement string) bool {
-	var i int
-	for _, s := range strings.Split(arrangement, OPERATIONAL) {
-		if len(s) == 0 {
-			continue
-		}
-		if i >= len(r.Groups) || len(s) != r.Groups[i] {
-			return false
-		}
-		i++
-
+	if s.CurrentGroup == len(r.Groups)-1 && s.DamagedCount == r.Groups[s.CurrentGroup] {
+		return true
 	}
-
-	return i == len(r.Groups)
+	return false
 }
 
 func part1() int {
@@ -81,6 +104,41 @@ func part1() int {
 			groups = append(groups, n)
 		}
 		records = append(records, &ConditionRecord{parts[0], groups})
+	}
+	var sum int
+	for _, r := range records {
+		sum += r.Arrangements()
+	}
+
+	return sum
+}
+
+func part2() int {
+	file, err := os.Open("input")
+	if err != nil {
+		panic("cannot open file")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var records []*ConditionRecord
+	for scanner.Scan() {
+		parts := strings.Fields(scanner.Text())
+		p1 := strings.Repeat(parts[0]+"?", 5)
+		p1 = p1[0 : len(p1)-1]
+
+		var groups []int
+		for range 5 {
+			for _, c := range strings.Split(parts[1], ",") {
+				n, err := strconv.Atoi(c)
+				if err != nil {
+					panic("cannot convert to int")
+				}
+				groups = append(groups, n)
+			}
+		}
+		records = append(records, &ConditionRecord{p1, groups})
 	}
 	var sum int
 	for _, r := range records {
