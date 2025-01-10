c3b33eb54e8da79fecff4ded5bbc38a14dc1c2e8
Walerian <waleria.sobczak@gmail.com>
2025-01-10 16:05:33 +0900

    2023: Day 18. Part 2.

--- 2023/day-18/lavaduct_lagoon.go
+++ 2023/day-18/lavaduct_lagoon.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log/slog"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -11,6 +12,7 @@ import (
 
 func main() {
 	fmt.Println(part1())
+	fmt.Println(part2())
 }
 
 func part1() int {
@@ -27,10 +29,8 @@ func part1() int {
 	}()
 
 	scanner := bufio.NewScanner(file)
-	grid := make(map[complex128]struct{})
 	coord := 0 + 0i
 	var corners []complex128
-	var maxX, maxY float64
 
 	for scanner.Scan() {
 		parts := strings.Fields(scanner.Text())
@@ -39,16 +39,7 @@ func part1() int {
 		if err != nil {
 			panic(fmt.Sprintf("cannot convert %s to int", parts[1]))
 		}
-		for range len {
-			coord = nextCoord(coord, dir)
-			grid[coord] = struct{}{}
-		}
-		if real(coord) > maxX {
-			maxX = real(coord)
-		}
-		if imag(coord) > maxY {
-			maxY = imag(coord)
-		}
+		coord = nextCoord(coord, dir, float64(len))
 		corners = append(corners, coord)
 	}
 
@@ -56,25 +47,62 @@ func part1() int {
 		logger.Error("Scanner error", "error", err)
 	}
 
-	return int(area(corners)) + len(grid)/2 + 1
+	return area(corners) + perimeter(corners)/2 + 1
 }
 
-func nextCoord(coord complex128, dir byte) complex128 {
+func part2() int {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	file, err := os.Open("input")
+	if err != nil {
+		panic("cannot open file")
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			logger.Error("Failed to close the file", "error", err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+	coord := 0 + 0i
+	var corners []complex128
+	dirs := map[byte]byte{'0': 'R', '1': 'D', '2': 'L', '3': 'U'}
+
+	for scanner.Scan() {
+		instruction := strings.Fields(scanner.Text())[2]
+		len, err := strconv.ParseInt(instruction[2:7], 16, 0)
+		if err != nil {
+			panic(fmt.Sprintf("cannot convert %s to int", instruction[:6]))
+		}
+		dir := dirs[instruction[7]]
+
+		coord = nextCoord(coord, dir, float64(len))
+		corners = append(corners, coord)
+	}
+
+	if err := scanner.Err(); err != nil {
+		logger.Error("Scanner error", "error", err)
+	}
+
+	return area(corners) + perimeter(corners)/2 + 1
+}
+
+func nextCoord(coord complex128, dir byte, delta float64) complex128 {
 	switch dir {
 	case 'U':
-		return coord - 1 + 0i
+		return coord - complex(delta, 0)
 	case 'R':
-		return coord + 0 + 1i
+		return coord + complex(0, delta)
 	case 'D':
-		return coord + 1 + 0i
+		return coord + complex(delta, 0)
 	case 'L':
-		return coord + 0 - 1i
+		return coord - complex(0, delta)
 	default:
 		panic("unknown direction")
 	}
 }
 
-func area(cs []complex128) float64 {
+func area(cs []complex128) int {
 	var res float64
 	for i := 0; i < len(cs); i++ {
 		c1, c2 := cs[i], cs[(i+1)%len(cs)]
@@ -85,5 +113,19 @@ func area(cs []complex128) float64 {
 		res = -res
 	}
 
-	return res / 2
+	return int(res / 2)
+}
+
+func perimeter(cs []complex128) int {
+	var res float64
+	for i := 0; i < len(cs); i++ {
+		c1, c2 := cs[i], cs[(i+1)%len(cs)]
+		res += math.Sqrt(math.Pow(real(c2)-real(c1), 2) + math.Pow(imag(c2)-imag(c1), 2))
+	}
+
+	if res < 0 {
+		res = -res
+	}
+
+	return int(res)
 }
