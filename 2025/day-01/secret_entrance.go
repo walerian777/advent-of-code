a3619226f666dc4a4380d1862db8de2bd2526c47
Walerian <walerian.sobczak@gmail.com>
2025-12-13 09:36:29 +0900

    2025: Day 1. Part 2.

--- 2025/day-01/secret_entrance.go
+++ 2025/day-01/secret_entrance.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"bufio"
 	"fmt"
@@ -8,9 +7,9 @@ import (
 	"strconv"
 )
 
-
 func main() {
 	fmt.Println(part1())
+	fmt.Println(part2())
 }
 
 func part1() int {
@@ -50,6 +49,77 @@ func part1() int {
 	return sum
 }
 
+func part2() int {
+	file, err := os.Open("input")
+	if err != nil {
+		panic("cannot open file")
+	}
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			panic("cannot close file")
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+	dial := 50
+	var count, sum int
+
+	for scanner.Scan() {
+		l := scanner.Text()
+		dir := l[0]
+		val, err := strconv.Atoi(l[1:])
+		if err != nil {
+			panic("cannot convert to int")
+		}
+		count, dial = rotateAndCount(dir, dial, val)
+		sum += count
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic("scanner error")
+	}
+
+	return sum
+}
+
+func rotateAndCount(dir byte, dial, val int) (int, int) {
+	var count int
+	if dir == 'R' {
+		newDial := dial + val
+		if newDial >= 100 {
+			count += newDial / 100
+		}
+		if newDial == 0 {
+			count += 1
+		}
+		return count, newDial % 100
+	} else {
+		if dial == 0 {
+			dial = 100
+		}
+		newDial := dial - val
+		if newDial == 0 {
+			count += 1
+		}
+		if newDial < 0 {
+			div := newDial / -100
+			if newDial+div*100 < 0 {
+				div += 1
+			}
+			count += div
+			adjustedDial := newDial + div*100
+			if adjustedDial == 0 {
+				count += 1
+			}
+			return count, adjustedDial
+		} else {
+			return count, newDial
+		}
+	}
+}
+
 func rotate(dir byte, dial, val int) int {
 	if dir == 'R' {
 		return (dial + val) % 100
@@ -57,7 +127,7 @@ func rotate(dir byte, dial, val int) int {
 		x := dial - val
 		if x < 0 {
 			div := x / -100
-			return x + div * 100
+			return x + div*100
 		} else {
 			return x
 		}
