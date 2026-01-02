3a49fde57f9eaf4741c76c2f19d7dd34d810d9bb
Walerian <walerian.sobczak@gmail.com>
2026-01-02 15:33:56 +0900

    2025: Day 4. Part 2.

--- 2025/day-04/printing_department.go
+++ 2025/day-04/printing_department.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	fmt.Println(part1())
+	fmt.Println(part2())
 }
 
 type Coord struct {
@@ -63,6 +64,64 @@ func part1() int {
 	return rolls
 }
 
+func part2() int {
+	file, err := os.Open("input")
+	if err != nil {
+		panic("cannot open file")
+	}
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			panic("cannot close file")
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+	var diagram [][]byte
+	var rolls int
+
+	for scanner.Scan() {
+		l := scanner.Text()
+		diagram = append(diagram, []byte(l))
+	}
+
+	rows := len(diagram)
+	cols := len(diagram[0])
+	neighbors := neighborsFuncGenerator(rows, cols)
+
+	for {
+		removed := 0
+		for x := 0; x < rows; x++ {
+			for y := 0; y < cols; y++ {
+				if diagram[x][y] != '@' {
+					continue
+				}
+				cnt := 0
+				ns := neighbors(x, y)
+				for _, n := range ns {
+					if diagram[n.X][n.Y] == '@' {
+						cnt++
+						if cnt >= 4 {
+							break
+						}
+					}
+				}
+				if cnt < 4 {
+					diagram[x][y] = 'x'
+					rolls++
+					removed++
+				}
+			}
+		}
+		if removed == 0 {
+			break
+		}
+	}
+
+	return rolls
+}
+
 func neighborsFuncGenerator(maxX, maxY int) func(int, int) []Coord {
 	return func(x, y int) []Coord {
 		return neighborsWithLimits(x, y, maxX, maxY)
@@ -77,7 +136,7 @@ func neighborsWithLimits(x, y, maxX, maxY int) []Coord {
 	}
 	var valid []Coord
 	for _, d := range deltas {
-		newX, newY := x + d.X, y + d.Y
+		newX, newY := x+d.X, y+d.Y
 		if newX < 0 || newX >= maxX {
 			continue
 		}
