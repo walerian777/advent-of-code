452574134378352793f85b9a77bdd175149086ec
Walerian <walerian.sobczak@gmail.com>
2026-01-05 22:36:20 +0900

    2025: Day 7. Part 1.

---
+++ 2025/day-07/laboratories.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println(part1())
+}
+
+func part1() int {
+	file, err := os.Open("input")
+	if err != nil {
+		panic("cannot open file")
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic("cannot close file")
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+	var diagram [][]byte
+
+	for scanner.Scan() {
+		diagram = append(diagram, []byte(scanner.Text()))
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic("scanner error")
+	}
+
+	atPos := func(p [2]int) byte {
+		return diagram[p[0]][p[1]]
+	}
+
+	isValid := isValidGenerator(len(diagram), len(diagram[0]))
+
+	start := bytes.IndexByte(diagram[0], 'S')
+	var beams [][2]int
+	beams = append(beams, toPoint(0, start))
+	var cnt int
+	for len(beams) > 0 {
+		visited := make(map[[2]int]struct{})
+		var newBeams [][2]int
+		for _, beam := range beams {
+			nextBeam := moveDown(beam)
+			if !isValid(nextBeam) {
+				continue
+			}
+			if atPos(nextBeam) == '^' {
+				cnt++
+				leftBeam := moveLeft(nextBeam)
+				rightBeam := moveRight(nextBeam)
+
+				if isValid(leftBeam) {
+					if _, ok := visited[leftBeam]; !ok {
+						visited[leftBeam] = struct{}{}
+						newBeams = append(newBeams, leftBeam)
+					}
+				}
+				if isValid(rightBeam) {
+					if _, ok := visited[rightBeam]; !ok {
+						visited[rightBeam] = struct{}{}
+						newBeams = append(newBeams, rightBeam)
+					}
+				}
+			} else {
+				if _, ok := visited[nextBeam]; !ok {
+					visited[nextBeam] = struct{}{}
+					newBeams = append(newBeams, nextBeam)
+				}
+			}
+		}
+		beams = newBeams
+	}
+	return cnt
+}
+
+func isValidGenerator(maxX, maxY int) func(p [2]int) bool {
+	return func(p [2]int) bool {
+		x, y := p[0], p[1]
+		if x < 0 || x >= maxX {
+			return false
+		}
+		if y < 0 || y >= maxY {
+			return false
+		}
+		return true
+	}
+}
+
+func toPoint(x, y int) [2]int {
+	return [2]int{x, y}
+}
+
+func moveDown(p [2]int) [2]int {
+	return [2]int{p[0] + 1, p[1]}
+}
+
+func moveLeft(p [2]int) [2]int {
+	return [2]int{p[0], p[1] - 1}
+}
+
+func moveRight(p [2]int) [2]int {
+	return [2]int{p[0], p[1] + 1}
+}
